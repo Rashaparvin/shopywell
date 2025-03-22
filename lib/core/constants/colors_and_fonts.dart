1c1f71a78bcd151d4bab0dc909a44276822ce0ac
rasha <mrasha141@gmail.com>
2025-03-22 20:19:03 +0530

    project started and splash screen completed

---
+++ lib/core/constants/colors_and_fonts.dart
@@ -0,0 +1,21 @@
+// Colors
+import 'package:flutter/material.dart';
+
+class Pallete {
+  // Common colors
+  static const Color kWhiteColor = Colors.white;
+  static const Color kRedColor = Colors.red;
+
+  // Text colors
+  static const Color kTextRedColor = Colors.red;
+  static const Color kTextGreyColor = Colors.grey;
+  static const Color kTextBlackColor = Colors.black;
+  static const Color kTextWhiteColor = Colors.white;
+}
+
+// Font Families
+class PoppinsFonts {
+  String kMontserratBold = 'MontserratBold';
+  String kMontserratNormal = 'MontserratNormal';
+  String kMontserratLight = 'MontserratLight';
+}
---
+++ lib/core/strings/image_strings.dart
@@ -0,0 +1,10 @@
+// App images in string format
+class Images {
+  // App logo
+  static const String appLogo = 'assets/images/shopywell_logo.png';
+
+  // On-boarding images
+  static const String onBoardingImage1 = 'assets/images/page_1.png';
+  static const String onBoardingImage2 = 'assets/images/page_2.png';
+  static const String onBoardingImage3 = 'assets/images/page_3.png';
+}
